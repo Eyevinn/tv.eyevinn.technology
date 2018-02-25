6155dda5ca4f7f24551bdbaa81b2561cdf5c9aaf
birme <jonas.birme@eyevinn.se>
2018-02-25 10:49:21 +0100

    Playlist support

--- backend/assetmanager/server.js
+++ backend/assetmanager/server.js
@@ -2,20 +2,27 @@ const restify = require('restify');
 const debug = require('debug')('assetmgr-server');
 
 const VODs = [
-  { id: 1, uri: "https://maitv-vod.lab.eyevinn.technology/streaming+tech+2017+HLS/Arash/Arash.m3u8" },
-  { id: 2, uri: "https://maitv-vod.lab.eyevinn.technology/streaming+tech+2017+HLS/Ozer/Ozer.m3u8" },
-  { id: 3, uri: "https://maitv-vod.lab.eyevinn.technology/streaming+tech+2017+HLS/Springhall_Jones/Springhall.m3u8" },
-  { id: 4, uri: "https://maitv-vod.lab.eyevinn.technology/streaming+tech+2017+HLS/Lindqvist_Skaneby/Lindqvist.m3u8" },
-  { id: 5, uri: "https://maitv-vod.lab.eyevinn.technology/streaming+tech+2017+HLS/Widlund/Widlund.m3u8" },
-  { id: 6, uri: "https://maitv-vod.lab.eyevinn.technology/stswe1/master.m3u8" },
-  { id: 7, uri: "https://maitv-vod.lab.eyevinn.technology/VINN.mp4/master.m3u8" }
+  { id: 1, title: "Vinngroup Promotion", uri: "https://maitv-vod.lab.eyevinn.technology/VINN.mp4/master.m3u8" },
+  { id: 2, title: "Tears of Steel", uri: "https://maitv-vod.lab.eyevinn.technology/tearsofsteel_4k.mov/master.m3u8" },
+  { id: 3, title: "Att jobba på Vinnter", uri: "https://maitv-vod.lab.eyevinn.technology/Attjobbapavinnter.mp4/master.m3u8" },
+  { id: 4, title: "David Springall and Andy Jones at Streaming Tech Sweden 2017", uri: "https://maitv-vod.lab.eyevinn.technology/stswe17-springalljones.mp4/master.m3u8" },
+  { id: 5, title: "Jan Ozer at Streaming Tech Sweden 2017", uri: "https://maitv-vod.lab.eyevinn.technology/stswe17-ozer.mp4/master.m3u8" },
+  { id: 6, title: "Come Shine recording at Atlantis Studios", uri: "https://maitv-vod.lab.eyevinn.technology/ComeShineiatlantis.mp4/master.m3u8" },
+  { id: 7, title: "Reportage från Klassiker-garaget", uri: "https://maitv-vod.lab.eyevinn.technology/Klassiker_garaget.mp4/master.m3u8" },
+  { id: 8, title: "Marcus Lindén at Streaming Tech Sweden 2017", uri: "https://maitv-vod.lab.eyevinn.technology/stswe17-linden.mp4/master.m3u8" },
+];
+
+const PLAYLISTs = [
+  { id: "stswe17", title: "Streaming Tech Sweden 2017", assets: [4, 5, 8] },
+  { id: "skaneby", title: "Skaneby productions", assets: [3, 6, 7] },
 ];
 
 class AssetManager {
   constructor() {
     this.server = restify.createServer();
+    this.server.use(restify.plugins.queryParser());
 
-    this.server.get('/nextVod/random', this._handleNextVodByRandom.bind(this));
+    this.server.get('/nextVod/:playlist', this._handleNextVod.bind(this));
   }
 
   listen(port) {
@@ -24,11 +31,34 @@ class AssetManager {
     });
   }
 
-  _handleNextVodByRandom(req, res, next) {
-    const rndIdx = Math.floor(Math.random() * VODs.length);
-    res.send(200, VODs[rndIdx]);
+  _handleNextVod(req, res, next) {
+    debug(req.params);
+    debug(req.query);
+    if (req.params['playlist'] === 'random') {
+      const rndIdx = Math.floor(Math.random() * VODs.length);
+      res.send(200, VODs[rndIdx]);
+    } else {
+      const playlist = PLAYLISTs.find(p => p.id === req.params['playlist']);
+      const position = req.query['position'] || 0;
+      if (playlist) {
+        let assets = [];
+        let nextPos = position;
+        nextPos++;
+        if (nextPos > playlist.assets.length) {
+          nextPos = 0;
+        }
+        debug(nextPos);
+        playlist.assets.forEach(assetId => { assets.push(this._lookupVodById(assetId)); });
+        res.send(200, assets[nextPos]);
+      }
+    }
     next();
+  }
+
+  _lookupVodById(id) {
+    return VODs.find(v => v.id === id);
   }
 }
 
 module.exports = AssetManager;
+
